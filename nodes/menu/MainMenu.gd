1b71edbb3b042dcea96a92396bb9f8f2a2e7258f
JohnnyArachnid <daniel.szarek@student.uj.edu.pl>
2024-04-26 20:15:28 +0200

    Menu Działające

---
+++ nodes/menu/MainMenu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+@onready var start_button : Button = $MarginContainer/HBoxContainer/VBoxContainer/StartButton
+@onready var exit_button : Button = $MarginContainer/HBoxContainer/VBoxContainer/ExitButton
+@onready var start_level : PackedScene = preload("res://nodes/game/game.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	start_button.button_down.connect(on_start_pressed)
+	exit_button.button_down.connect(on_exit_pressed)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func on_start_pressed() -> void:
+	get_tree().change_scene_to_packed(start_level)
+	self.queue_free()
+	
+func on_exit_pressed() -> void:
+	get_tree().quit() 
