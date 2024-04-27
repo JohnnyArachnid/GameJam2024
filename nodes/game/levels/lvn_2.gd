58da03c7a9fc49a5641235c93d78e33c2d6e88bd
JohnnyArachnid <daniel.szarek@student.uj.edu.pl>
2024-04-27 13:04:01 +0200

    Required changes

--- nodes/game/levels/lvn_2.gd
+++ nodes/game/levels/lvn_2.gd
@@ -1,2 +1,14 @@
 extends GameManager
 
+@export var next_lvn : PackedScene
+@onready var timer : Timer = $Area2D/Timer
+
+func _on_area_2d_body_entered(body):
+	if (body.name == "player"):
+		transitioner.animation_player.play("fade_out")
+		timer.start()
+		pass
+		
+func _on_timer_timeout():
+	get_tree().change_scene_to_packed(next_lvn)
+	pass
