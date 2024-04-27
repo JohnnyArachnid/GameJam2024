ba3db63850b39d5901e7a43b9cd3d31308141210
JohnnyArachnid <daniel.szarek@student.uj.edu.pl>
2024-04-27 02:49:37 +0200

    FullScreen

--- nodes/menu/MainMenu.gd
+++ nodes/menu/MainMenu.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var start_button : Button = $MarginContainer/HBoxContainer/VBoxContainer/StartButton
 @onready var exit_button : Button = $MarginContainer/HBoxContainer/VBoxContainer/ExitButton
 @onready var start_level : PackedScene = preload("res://nodes/game/game.tscn")
+@onready var timer : Timer = $Timer
 
 
 # Called when the node enters the scene tree for the first time.
@@ -18,6 +19,9 @@ func _process(delta):
 func on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(start_level)
 	self.queue_free()
+	pass
+	
 	
 func on_exit_pressed() -> void:
 	get_tree().quit() 
+	
