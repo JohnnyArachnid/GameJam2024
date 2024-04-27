febfd5e7dab96311d800c95d652cdf67177c1cfb
JohnnyArachnid <daniel.szarek@student.uj.edu.pl>
2024-04-27 05:59:11 +0200

    Working back again

--- nodes/main/main.gd
+++ nodes/main/main.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
+@onready var menu : Control = $MainMenu
+@export var game : PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	menu.connect("play",startGame)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
+	
+func startGame():
+	get_tree().change_scene_to_packed(game)
 	pass
--- nodes/menu/MainMenu.gd
+++ nodes/menu/MainMenu.gd
@@ -2,9 +2,9 @@ extends Control
 
 @onready var start_button : Button = $MarginContainer/HBoxContainer/VBoxContainer/StartButton
 @onready var exit_button : Button = $MarginContainer/HBoxContainer/VBoxContainer/ExitButton
-@onready var start_level : PackedScene = preload("res://nodes/game/game.tscn")
 @onready var timer : Timer = $Timer
 
+signal play
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,8 +17,8 @@ func _process(delta):
 	pass
 	
 func on_start_pressed() -> void:
-	get_tree().change_scene_to_packed(start_level)
 	self.queue_free()
+	play.emit()
 	pass
 	
 	
